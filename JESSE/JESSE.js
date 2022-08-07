50bf327ac87a2c725ebf59cdeca003c94c5693b8
Louan <90987010+blupblup46@users.noreply.github.com>
2022-08-07 21:18:06 +0200

    Update JESSE.js

--- JESSE/JESSE.js
+++ JESSE/JESSE.js
@@ -2,13 +2,13 @@ const JESSE = document.getElementById("JESSE")
 let index = 0;
 
 let sons = new Array()
-for (let x=1; x<9; x++){
+for (let x=0; x<10; x++){
   sons.push(new Audio("./JESSEsons/J"+x.toString()+".wav"))
 }
 
 JESSE.addEventListener("click", function(){
-    console.log(index)
+    console.log("J"+index.toString()+" est joué")
     sons[index].play()
     index+=1;
-    index%=8;
+    index%=10;
 })
