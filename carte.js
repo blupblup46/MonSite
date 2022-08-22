9f396737517ed93cafb29a2cc96f7978949d1773
blupblup46 <louan46@gmail.com>
2022-08-22 15:05:25 +0200

    suppression message site en construction

--- carte.js
+++ carte.js
@@ -2,6 +2,7 @@ let toast;
 const fermer = document.getElementsByClassName("fermer")
 const rideau = document.getElementById('rideau');
 
+/* plus qu'un element de classe 'fermer' après suppression du message 'site en construction'
 fermer[0].addEventListener("click", function(){
     console.log("ferme")
     document.getElementById("WIP").style.display = "none";
@@ -14,7 +15,14 @@ fermer[1].addEventListener("click", function(){
   rideau.style.display = "none";
 
 })
+*/
 
+fermer[0].addEventListener("click", function(){
+  console.log("ferme")
+  toast.hide()
+  rideau.style.display = "none";
+
+})
 
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
