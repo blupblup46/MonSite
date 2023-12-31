6db15242095e89b782fcaa9aaf8488c9228fadb0
blupblup <louan46@gmail.com>
2023-12-31 20:20:16 +0100

    Making first page
    Making intro page and make it responsive

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,13 +1,35 @@
 const activePage = document.getElementsByTagName("main")[0].getAttribute("activePage") as string;
 const details = document.querySelectorAll("details");
+const burgerMenuButton = document.querySelector("body>div") as HTMLElement;
+const nav = document.querySelector("body>nav") as HTMLElement;
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+
+
 enum CssClasses  {
-  underlined = "underlined"
+  underlined = "underlined",
+  isDisplayed = "isDisplayed",
+  hidden = "hidden",
+  visible = "visible"
+
 }
 
+navHidder();
 document.getElementById(activePage)?.classList.add(CssClasses.underlined);
 
 details.forEach(d => d.addEventListener("toggle", onDetailsOpen));
+burgerMenuButton?.addEventListener("click",() => {
+  console.log("click");
+  if(nav.classList.contains(CssClasses.hidden)){
+    nav.classList.remove(CssClasses.hidden);
+    nav.classList.add(CssClasses.visible);
+  } else{
+    nav.classList.remove(CssClasses.visible);
+    nav.classList.add(CssClasses.hidden);
+  } 
 
+}
+);
 
 function onDetailsOpen(){
   if(this.open){
@@ -18,3 +40,16 @@ function onDetailsOpen(){
     });
   }
 };
+
+window.addEventListener('resize', navHidder);
+
+function navHidder(){
+  if(viewportHeight < 600 || viewportWidth < 1000){
+    nav.classList.remove(CssClasses.visible) ;
+    nav.classList.add(CssClasses.hidden);
+  
+  }else{
+    nav.classList.add(CssClasses.visible) ;
+    nav.classList.remove(CssClasses.hidden);
+  }
+}
